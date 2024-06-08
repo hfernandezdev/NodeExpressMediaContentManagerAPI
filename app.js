0ccf3f49dbeffe46f8d969a261e7d1f0ba7121b7
Humberto Fernández <humbertof44@gmail.com>
2024-06-08 13:07:56 -0400

    Add the connection to the database

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/media_content_manager_db', { auth: { username: "admin", password: "1234" } })
+  .then(() => console.log('Conexión a MongoDB establecida'))
+  .catch(err => console.error('Error al conectar a MongoDB:', err));
 
 app.use(logger('dev'));
 app.use(express.json());
